c8c41a63cb395dabbb1197c25bbe921c1254f715
Connor Botts <connor.botts@gmail.com>
2016-09-16 18:16:23 -0400

    Initial Prototype
    Car driving and braking is there, it doesnt feel all that great yet and
    there is no camera follow

---
+++ Assets/scripts/CarControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CarControl : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    public float maxBrakingTorque; //how fast should you brake
+
+    private float x_Input;
+    private float accelerationForce = 0;
+    private float brakingForce = 0;
+
+    private void Update()
+    {
+        if (Input.GetButton("Brake")) brakingForce = 1;
+        else brakingForce = 0;
+        if (Input.GetButton("Accelerate")) accelerationForce = 1;
+        else accelerationForce = 0;
+
+        x_Input = Input.GetAxis("Horizontal");
+    }
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * accelerationForce;
+        float steering = maxSteeringAngle * x_Input;
+
+        foreach (AxleInfo axleInfo in axleInfos)
+        {
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            axleInfo.leftWheel.brakeTorque = brakingForce * maxBrakingTorque;
+            axleInfo.rightWheel.brakeTorque = brakingForce * maxBrakingTorque;
+        }
+    }
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
+}
