05cb9759c43b5a2ad21dd632fd91c0c8e623c071
Connor Botts <connor.botts@gmail.com>
2016-09-27 23:02:56 -0400

    Shitty Alpha!

--- Assets/scripts/CarControl.cs
+++ Assets/scripts/CarControl.cs
@@ -10,25 +10,34 @@ public class CarControl : MonoBehaviour
     public float maxBrakingTorque; //how fast should you brake
     public int antiRollValue; //prevents car from flipping
     public int currentPlayer = 0;
+    public int carHealth = 100;
     public float controllerDeadzone = 0.15f;
+    public Vector3 newCom;
 
     private Vector2 x_Input;
     private float accelerationForce = 0;
     private float brakingForce = 0;
     private Rigidbody rigid;
     private int mph;
+    private Vector3 originCom;
 
     public DataManager data;
 
     private void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        originCom = rigid.centerOfMass;
     }
 
     private void Update()
     {
+        if(carHealth <= 0)
+        {
+            Debug.Log("KABOOM. Your car blew up! GG.");
+            rigid.AddExplosionForce(50000, transform.position, 500);
+        }
         brakingForce = -Input.GetAxis("Brake" + currentPlayer);
-        accelerationForce = Input.GetAxis("Accelerate" + currentPlayer);
+        accelerationForce = Mathf.Clamp(Input.GetAxis("Accelerate" + currentPlayer), 0.2f, 1.0f);
         x_Input = new Vector2(Input.GetAxis("Horizontal" + currentPlayer), Input.GetAxis("Vertical" + currentPlayer));
         if (x_Input.magnitude < controllerDeadzone)
             x_Input = Vector2.zero;
@@ -58,7 +67,6 @@ public class CarControl : MonoBehaviour
     {
         mph = (int)((rigid.velocity.magnitude * 10) / 2.5);
         data.CurrentMPH = mph;
-        // Debug.Log("MPH:" + mph);
         float motor = maxMotorTorque * accelerationForce;
         float steering = maxSteeringAngle * x_Input.x;
 
@@ -78,7 +86,14 @@ public class CarControl : MonoBehaviour
             axleInfo.rightWheel.brakeTorque = brakingForce * maxBrakingTorque;
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
-
+            if(!axleInfo.leftWheel.isGrounded && !axleInfo.rightWheel.isGrounded)
+            {
+                rigid.centerOfMass = newCom;
+            }
+            else
+            {
+                rigid.centerOfMass = originCom;
+            }
             //WheelHit hit = new WheelHit();
             //float travelL = 1f;
             //float travelR = 1f;
@@ -89,6 +104,33 @@ public class CarControl : MonoBehaviour
             //float antiRollForce = (travelL - travelR) * antiRollValue;
             //if (groundedL) rigid.AddForceAtPosition(axleInfo.leftWheel.transform.up * -antiRollForce, axleInfo.leftWheel.transform.position);
             //if (groundedR) rigid.AddForceAtPosition(axleInfo.rightWheel.transform.up * -antiRollForce, axleInfo.rightWheel.transform.position);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(mph > 0 && mph < 50)
+        {
+            Debug.Log("Boop. No Damage.");
+        }
+        else if(mph > 51 && mph < 80)
+        {
+            Debug.Log("Bonk. Minor Damage");
+            carHealth -= 20;
+        }
+        else if(mph > 81 && mph < 110)
+        {
+            Debug.Log("CRRCH. Medium Damage");
+            carHealth -= 40;
+        }
+        else if(mph > 111 && mph < 130)
+        {
+            Debug.Log("REEEEEEEEEEEEGFEGWFHE. High Damage");
+            carHealth -= 60;
+        }
+        else
+        {
+            carHealth -= 100;
         }
     }
 }
