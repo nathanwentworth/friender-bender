b4ec765ef791667b88803220524419dbf36fdae7
Nathan Wentworth <nwentworth95@gmail.com>
2016-10-05 16:38:19 -0400

    modified event system to lookup os

--- Assets/scripts/UIControlSet.cs
+++ Assets/scripts/UIControlSet.cs
@@ -6,10 +6,14 @@ public class UIControlSet : MonoBehaviour {
 
 	void Awake () {
     #if UNITY_STANDALONE_OSX || UNITY_EDITOR_OSX
+			GetComponent<StandaloneInputModule>().horizontalAxis = "Horizontal0_mac";
+			GetComponent<StandaloneInputModule>().verticalAxis = "Vertical0_mac";
 			GetComponent<StandaloneInputModule>().cancelButton = "Cancel_mac";
 			GetComponent<StandaloneInputModule>().submitButton = "Submit_mac";
   	#endif
     #if UNITY_STANDALONE_WIN || UNITY_EDITOR_WIN
+			GetComponent<StandaloneInputModule>().horizontalAxis = "Horizontal0_win";
+			GetComponent<StandaloneInputModule>().verticalAxis = "Vertical0_win";
 			GetComponent<StandaloneInputModule>().cancelButton = "Cancel_win";
 			GetComponent<StandaloneInputModule>().submitButton = "Submit_win";
   	#endif
