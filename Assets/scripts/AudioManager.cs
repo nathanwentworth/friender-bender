735cdbea31ea1a8fba0d316226d164f7827be4ef
Ethan Thibault <wickedlystudios@gmail.com>
2016-10-10 20:52:44 -0400

    First Implementation of Unity Audio
    Created first AudioManager Object, got background music and non-pitch
    changing engine

---
+++ Assets/scripts/AudioManager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour
+{
+	/*How can I make it so that the AudioManager detects all the audio sources attached to the object? My thinking being that if it can detect that, then it can know what functions and what don't, without having to manually wire them up every blody time?*/
+}
