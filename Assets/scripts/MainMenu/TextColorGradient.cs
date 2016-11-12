cea279648862f774c83800f85b242f61cf7b842c
Nathan Wentworth <nwentworth95@gmail.com>
2016-11-12 18:52:05 -0500

    text color gradient cycle

---
+++ Assets/scripts/MainMenu/TextColorGradient.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextColorGradient : MonoBehaviour {
+
+	public Gradient gradient;
+
+	private void OnEnable() {
+		StartCoroutine("ColorCycle");
+	}
+
+	private IEnumerator ColorCycle() {
+    float timer = 0;
+    while (true) {
+      timer += .01f;
+      if (timer >= 1) timer = 0;
+      GetComponent<Text>().color = gradient.Evaluate(timer);
+      yield return null;
+    }
+	}
+
+}
